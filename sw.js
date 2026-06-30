147ad62ca8e41e5feae0f956f043f04ae2ce063e
marry0114 <canon20110411@gmail.com>
2026-06-30 17:58:48 +0900

    Update service worker cache version and simplify fetch

--- sw.js
+++ sw.js
@@ -1,35 +1,18 @@
-const CACHE_NAME = 'kendo-score-v3';
+const CACHE_NAME = 'kendo-score-v4';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
-  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
-      return response;
-    }).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
