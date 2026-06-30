5f420ae51ad2c5274e82d6b21f1d4740088b9955
marry0114 <canon20110411@gmail.com>
2026-06-30 18:10:07 +0900

    Update service worker for cache versioning and fetching

--- sw.js
+++ sw.js
@@ -1,18 +1,37 @@
-const CACHE_NAME = 'kendo-score-v4';
+const CACHE_NAME = 'kendo-score-v5';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
